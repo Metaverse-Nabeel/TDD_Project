fabdeae712fc6b65ece9d199799a7fb97bce4abb
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-06-28 23:59:14 +0500

    Add test for Fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,22 +4,22 @@ describe Fizzbuzz do
   describe '#fizzbuzz' do
     include Fizzbuzz
 
-    it 'should return "fizz" when the number is divisible by 3' do
+    it 'should return "fizz" when the number is divisible by 3 correctly' do
       result = fizzbuzz(12)
       expect(result).to eq('fizz')
     end
 
-    it 'should return "buzz" when the number is divisible by 5' do
+    it 'should return "buzz" when the number is divisible by 5 correctly' do
       result = fizzbuzz(20)
       expect(result).to eq('buzz')
     end
 
-    it 'should return "fizzbuzz" when the number is divisible by both 3 and 5' do
+    it 'should return "fizzbuzz" when the number is divisible by both 3 and 5 correctly' do
       result = fizzbuzz(30)
       expect(result).to eq('fizzbuzz')
     end
 
-    it 'should return the number as a string when it is not divisible by 3 or 5' do
+    it 'should return the number as a string when it is not divisible by 3 or 5 correctly' do
       result = fizzbuzz(16)
       expect(result).to eq('16')
     end
