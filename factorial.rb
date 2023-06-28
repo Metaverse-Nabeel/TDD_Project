cee109d0ce5662dfef8e979cf01da2a3f43d1a9d
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-06-29 00:23:36 +0500

    Correct linter errors

--- factorial.rb
+++ factorial.rb
@@ -2,6 +2,7 @@ class Factorial
   def find(number)
     raise ArgumentError, 'Number must be greater than or equal to 0.' if number.negative?
     return 1 if number.zero?
+
     (1..number).reduce(:*)
   end
 end
