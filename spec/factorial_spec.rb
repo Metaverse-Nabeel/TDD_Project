f85cf7af3fb5a2b77882fdfe9ab55135a4f6bcce
Daniel <carreraprogrammer@gmail.com>
2023-06-29 12:19:53 -0500

    Fix factorial spec

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,20 +1,19 @@
-require_relative '../factorial'
+require_relative '../solver'
 
-describe Factorial do
-  context '#find' do
-    it 'should return the factorial of a positive number' do
-      calculator = Factorial.new
-      expect(calculator.find(5)).to eq(120)
+describe Solver do
+  let(:solver) { Solver.new }
+
+  describe '#factorial' do
+    it 'returns 1 for factorial(0)' do
+      expect(solver.factorial(0)).to eq(1)
     end
 
-    it 'should return 1 as the factorial of zero' do
-      calculator = Factorial.new
-      expect(calculator.find(0)).to eq(1)
+    it 'returns 120 for factorial(5)' do
+      expect(solver.factorial(5)).to eq(120)
     end
 
-    it 'should raise an error when given a negative number' do
-      calculator = Factorial.new
-      expect { calculator.find(-1) }.to raise_error(ArgumentError, 'Number must be greater than or equal to 0.')
+    it 'raises an error for factorial(-1)' do
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError, "Factorial is undefined for negative numbers.")
     end
   end
 end
